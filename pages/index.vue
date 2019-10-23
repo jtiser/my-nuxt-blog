50649eba033603f32e0de53f98f7117c95dc09e9
Jtiser <julien.tiser@outlook.com>
2019-10-23 14:10:15 +0200

    :lipstick: changed home page image

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="home-page">
-    <section class="intro">
-      <h1>Get the latest tech news!</h1>
-    </section>
+    <base-card dark>
+      <v-img
+        class="grey lighten-2"
+        height="400"
+        width="100%"
+        :src="require('@/assets/articles/blurcamera.jpg')"
+      >
+        <v-layout fill-height align-center pa-3>
+          <v-flex xs12 md7 offset-md5>
+            <h1 class="display-3 font-weight-light">Get the latest tech news</h1>
+            <div class="subheading text-uppercase pl-2 mb-4">Work in progress, come back later :)</div>
+          </v-flex>
+        </v-layout>
+      </v-img>
+    </base-card>
     <PostList />
   </div>
 </template>
