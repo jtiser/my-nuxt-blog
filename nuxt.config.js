375557e1d78c709955b6cf0bd1a0e73bf8421341
Jtiser <julien.tiser@outlook.com>
2019-11-02 15:03:21 +0100

    :sparkles: added a global date filter

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/base'],
+  plugins: ['~/plugins/base', '~/plugins/date-filter'],
   /*
    ** Nuxt.js dev-modules
    */
