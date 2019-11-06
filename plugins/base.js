a6a310468986b767451d700a9176a27b9109e81b
Jtiser <julien.tiser@outlook.com>
2019-11-06 21:27:56 +0100

    :bug: fixed missing base btn component

--- plugins/base.js
+++ plugins/base.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
 import BaseCard from '@/components/base/Card'
+import BaseBtn from '@/components/base/Btn'
 
 Vue.component(BaseCard.name, BaseCard)
+Vue.component(BaseBtn.name, BaseBtn)
