dfeed572e78791494c6e677cb32ae2280699479c
Jtiser <julien.tiser@outlook.com>
2019-11-06 21:23:22 +0100

    :art: extracted the nav bar to a proper component

---
+++ components/Core/NavigationDrawer.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-navigation-drawer v-model="drawer" temporary app>
+    <v-list>
+      <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+        <v-list-item-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: {
+    drawer: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Blog',
+          to: '/posts'
+        },
+        {
+          icon: 'mdi-information',
+          title: 'About',
+          to: '/about'
+        },
+        {
+          icon: 'mdi-account-settings',
+          title: 'Admin',
+          to: '/admin'
+        }
+      ]
+    }
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,8 @@
 <template>
   <v-app dark>
-    <core-app-bar :title="title" :drawer="drawer" @nav-icon-clicked="drawer=!drawer"></core-app-bar>
-    <v-navigation-drawer v-model="drawer" temporary app>
-      <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <core-app-bar :title="title" :drawer="drawer" @nav-icon-clicked="drawer=!drawer" />
+
+    <core-navigation-drawer :drawer="drawer" />
 
     <v-flex ref="scroll" class="main-container" id="scrolling-techniques">
       <v-content class="content">
@@ -29,38 +19,12 @@
 export default {
   components: {
     CoreFooter: () => import('@/components/core/Footer'),
-    CoreAppBar: () => import('@/components/core/AppBar')
+    CoreAppBar: () => import('@/components/core/AppBar'),
+    CoreNavigationDrawer: () => import('@/components/core/NavigationDrawer')
   },
   data() {
     return {
-      clipped: true,
       drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Blog',
-          to: '/posts'
-        },
-        {
-          icon: 'mdi-information',
-          title: 'About',
-          to: '/about'
-        },
-        {
-          icon: 'mdi-account-settings',
-          title: 'Admin',
-          to: '/admin'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
       title: 'My blog using Nuxt'
     }
   }
