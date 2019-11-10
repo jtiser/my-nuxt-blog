a2e8dc6c4f6dae197c577b199b40146193696e28
Jtiser <julien.tiser@outlook.com>
2019-11-10 15:24:42 +0100

    :lipstick: added a background image to the app bar

--- components/Core/AppBar/index.vue
+++ components/Core/AppBar/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <v-app-bar fixed dark color="primary darken-2" shrink-on-scroll>
-    <!-- <template v-slot:img="{ props }">
-      <v-img v-bind="props"></v-img>
-    </template>-->
+  <v-app-bar
+    fixed
+    dark
+    color="primary darken-2"
+    shrink-on-scroll
+    :src="require(`~/assets/images/space02.jpeg`)"
+  >
+    <template v-slot:img="{ props }">
+      <v-img v-bind="props" gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"></v-img>
+    </template>
 
     <v-app-bar-nav-icon @click.stop="$emit('nav-icon-clicked')"></v-app-bar-nav-icon>
 
