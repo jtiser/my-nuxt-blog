7d35a2339f92984a9bf60715c006433c975bded1
Jtiser <julien.tiser@outlook.com>
2019-11-10 17:08:20 +0100

    :iphone: fixed responsive design

--- components/Core/AppBar/index.vue
+++ components/Core/AppBar/index.vue
@@ -10,14 +10,14 @@
       <v-img v-bind="props" gradient="to top right, rgba(19,84,122,.5), #4a148c"></v-img>
     </template>
 
-    <v-app-bar-nav-icon class="d-lg-none" @click.stop="$emit('nav-icon-clicked')"></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon class="d-md-none" @click.stop="$emit('nav-icon-clicked')"></v-app-bar-nav-icon>
 
     <v-toolbar-title>{{title}}</v-toolbar-title>
 
     <v-spacer></v-spacer>
-    <search-bar v-show="breakpointDisplayReached" ref="search"></search-bar>
-    <link-btn v-show="breakpointDisplayReached" class="ml-2" link="/">Home</link-btn>
-    <link-btn v-show="breakpointDisplayReached" class="ml-2" link="/about">About</link-btn>
+    <search-bar class="d-none d-md-flex" ref="search"></search-bar>
+    <link-btn class="ml-2 d-none d-md-flex" link="/">Home</link-btn>
+    <link-btn class="ml-2 d-none d-md-flex" link="/about">About</link-btn>
   </v-app-bar>
 </template>
 
@@ -36,7 +36,7 @@ export default {
   },
   computed: {
     breakpointDisplayReached() {
-      return this.$vuetify.breakpoint.lg
+      return this.$vuetify.breakpoint.md
     }
   }
 }
