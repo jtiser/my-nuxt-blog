66c6c021747d0f405e0801bf252b3ae1bb9d5497
Jtiser <julien.tiser@outlook.com>
2019-11-10 15:01:10 +0100

    :art: removed dead code and files

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,5 @@
 <template>
-  <div>
-    <!-- <base-card dark>
-      <v-img
-        class="grey lighten-2"
-        height="400"
-        width="100%"
-        :src="require('@/assets/articles/blurcamera.jpg')"
-      >
-        <v-layout fill-height align-center pa-3>
-          <v-flex xs12 md7 offset-md5>
-            <h1 class="display-3 font-weight-light">Get the latest tech news</h1>
-            <div class="subheading text-uppercase pl-2 mb-4">Work in progress, come back later :)</div>
-          </v-flex>
-        </v-layout>
-      </v-img>
-    </base-card>-->
-    <PostList :posts="loadedPosts" />
-  </div>
+  <PostList :posts="loadedPosts" />
 </template>
 
 <script>
