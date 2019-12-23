f750388c891ab9b61b1f2f766b52334358805ffe
Jtiser <julien.tiser@outlook.com>
2019-12-23 12:13:22 +0100

    :sparkles: link button build using prismic

--- components/Core/AppBar/LinkBtn.vue
+++ components/Core/AppBar/LinkBtn.vue
@@ -1,17 +1,31 @@
 <template>
-  <v-btn text depressed v-bind="$attrs" v-on="$listeners" nuxt :to="link">
+  <v-btn
+    class="ml-2 d-none d-md-flex"
+    text
+    depressed
+    v-bind="$attrs"
+    v-on="$listeners"
+    nuxt
+    :to="to"
+  >
     <slot />
   </v-btn>
 </template>
 
 <script>
+import PrismicDOM from 'prismic-dom'
+import linkResolver from '~/plugins/link-resolver'
 export default {
   name: 'LinkBtn',
   inheritAttrs: false,
   props: {
     link: {
-      type: String,
-      defaul: '/'
+      type: Object
+    }
+  },
+  computed: {
+    to() {
+      return PrismicDOM.Link.url(this.link, linkResolver)
     }
   }
 }
--- components/Core/AppBar/index.vue
+++ components/Core/AppBar/index.vue
@@ -1,27 +1,34 @@
 <template>
-  <v-app-bar fixed dark color="primary darken-2" shrink-on-scroll :src="backgroundImg">
+  <v-app-bar
+    fixed
+    dark
+    color="primary darken-2"
+    shrink-on-scroll
+    :src="backgroundImg"
+  >
     <template v-slot:img="{ props }">
-      <v-img v-bind="props" gradient="to top right, rgba(19,84,122,.5), #4a148c"></v-img>
+      <v-img
+        v-bind="props"
+        gradient="to top right, rgba(19,84,122,.5), #4a148c"
+      ></v-img>
     </template>
 
-    <v-app-bar-nav-icon class="d-md-none" @click.stop="$emit('nav-icon-clicked')"></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon
+      class="d-md-none"
+      @click.stop="$emit('nav-icon-clicked')"
+    ></v-app-bar-nav-icon>
 
     <v-toolbar-title>{{ title }}</v-toolbar-title>
 
     <v-spacer></v-spacer>
     <search-bar class="d-none d-md-flex" ref="search"></search-bar>
 
-    <prismic-link
+    <link-btn
       v-for="({ link, label }, index) in navItems"
       :key="index"
-      class="header-nav-link"
-      :field="link"
+      :link="link"
+      >{{ label }}</link-btn
     >
-      <link-btn class="ml-2 d-none d-md-flex">{{label}}</link-btn>
-    </prismic-link>
-
-    <link-btn class="ml-2 d-none d-md-flex" link="/">Home</link-btn>
-    <link-btn class="ml-2 d-none d-md-flex" link="/about">About</link-btn>
   </v-app-bar>
 </template>
 
