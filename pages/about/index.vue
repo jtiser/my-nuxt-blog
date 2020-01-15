966eec81ed20961f47b8da4cd02c9a4a6e302843
Jtiser <julien.tiser@outlook.com>
2020-01-15 14:01:14 +0100

    ✨begining of 'about' feature

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,16 +1,56 @@
 <template>
-  <div class="about-page">
-    <h1>This is my absolutely fantastic "About Me" Page!</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-  </div>
+  <section class="about-page">
+    <v-card class="pa-5">
+      <v-card-title class="headline">{{ $prismic.richTextAsPlain(document.title) }}</v-card-title>
+      <!-- <slices-block :slices="slices" /> -->
+    </v-card>
+  </section>
 </template>
+
+<script>
+import Prismic from 'prismic-javascript'
+import PrismicConfig from '~/prismic.config.js'
+import SlicesBlock from '~/components/Posts/Slices/SlicesBlock'
+
+export default {
+  components: {
+    SlicesBlock
+  },
+  async asyncData({ params, error, req }) {
+    try {
+      // Query to get API object
+      const api = await Prismic.getApi(PrismicConfig.apiEndpoint, {})
+
+      // Query to get about content
+      const about = await api.getSingle('about')
+      // Load the edit button
+      return {
+        document: about.data,
+        slices: about.data.contenu
+      }
+    } catch (e) {
+      error({ statusCode: 404, message: 'Page not found' })
+    }
+  }
+}
+</script>
+
 
 <style scoped>
 .about-page {
-  text-align: center;
-  padding: 30px;
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .about-page {
+    width: 700px;
+    margin: auto;
+  }
+}
+@media (min-width: 1024px) {
+  .about-page {
+    width: 1000px;
+    margin: auto;
+  }
 }
 </style>
